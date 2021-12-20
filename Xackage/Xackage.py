a6a2bf28050f38b2ab8186509b645edc9af7bca6
Thierry Bremard <t.bremard@gmail.com>
2021-12-20 18:02:22 +0100

    enumerate all 3rd party packages

--- Xackage/Xackage.py
+++ Xackage/Xackage.py
@@ -7,17 +7,23 @@ myLogger = MyLogger.MyLogger()
 myLogger.SetupLogger()
 _log = myLogger.GetLogger()
 _log.debug('##############################');
-_log.debug('##          Starting        ##');
-
+_log.debug('##  Starting Xackage        ##');
+_log.debug('##  Thierry Bremard         ##');
+_log.debug('##  t.bremard@gmail.com     ##');
+_log.debug('Download all ThirdParty packages from Android device.');
+_log.debug('Needs Android SDK to be installed and adb.exe in PATH');
 cmdAdb = 'adb'
 cmdOpenShell = 'adb shell'
 cmdCloseShell = 'exit'
-cmdListPackages = 'pm list packages -3'
+cmdListPackages = b'pm list packages -3'
 cmdGetPathOfApk = 'pm path com.skype.raider'
-
-#output=subprocess.run([shutil.which(cmdOpenShell), cmdListPackages, cmdCloseShell], capture_output=True, shell=True)
 myPath=shutil.which(cmdAdb)
-output=subprocess.run(myPath, capture_output=True, shell=True)
-#output=subprocess.run(['dir'], capture_output=True, shell=True)
+execResult=subprocess.run([myPath, 'shell'], input=cmdListPackages, capture_output=True, shell=True)
 _log.debug('##############################');
-print(output)
+packageBulk=execResult.stdout.decode('ascii')
+packageList = packageBulk.splitlines()
+validPackages=[]
+for package in packageList:
+    packageName=package.replace('package:', '')
+    validPackages.append(packageName)
+_log.debug('Identified '+ str(len(validPackages))+ ' packages')
